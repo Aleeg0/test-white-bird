82b7d1bfc45832c1e0cb5b1d9366923d0013f9ae
Aleeg0 <Sankafoxe@gmail.com>
2025-08-20 21:57:24 +0300

    (project): update App
    Add background img.
    Add some styles.
    Add layouts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import RouterLayout from "@/shared/router/RouterLayout.tsx";
+import "./App.scss";
+import ThemeLayout from "@/shared/layouts/themeLayout/themeLayout.tsx";
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <ThemeLayout>
+      <RouterLayout />
+    </ThemeLayout>
+  );
 }
 
-export default App
+export default App;
